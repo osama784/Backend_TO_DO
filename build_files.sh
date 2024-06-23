15a1cba9a9745e08c92691d0a363813006ac154d
Osama <osamadoage0@gmail.com>
2024-06-23 12:59:00 +0300

    modefy buil_files.sh to create virtual env

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,18 @@
+# build_files.sh
+echo "BUILD START"
+
+# Create a virtual environment
+python3.9 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+echo "BUILD END"
+
+
