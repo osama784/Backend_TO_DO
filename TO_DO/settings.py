88317b9649826ee985112ff7f108ae8a2bdf89d7
Osama <osamadoage0@gmail.com>
2024-06-23 12:27:07 +0300

    make changes to apply the staticfile

--- TO_DO/settings.py
+++ TO_DO/settings.py
@@ -122,12 +122,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
