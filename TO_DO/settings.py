8fc74785dae7955044b1e0ec9bffb7ecd185038a
Osama <osamadoage0@gmail.com>
2024-06-23 14:51:12 +0300

    modefying database location

--- TO_DO/settings.py
+++ TO_DO/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-r#j$x1-35=*a7@gecdf*0-x&61@-8!x^u@8)&+sqi4im4k@ddr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'TO_DO.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'tmp' / 'db.sqlite3',
     }
 }
 
